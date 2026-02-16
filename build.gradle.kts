05b284aadd411247d1e77dc906ea1e67344ea37f
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 20:04:50 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("com.android.tools.build:gradle:8.2.2")
         classpath("com.aliucord:gradle:main-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     }
 }
 
