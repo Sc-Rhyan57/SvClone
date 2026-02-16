8a74fab3c5d7c87568bd7139fa2bfa6904201746
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 17:14:33 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,61 @@
-plugins {
-    id("com.aliucord.gradle") version "1.2.0" apply false
-    id("com.android.library") version "7.4.2" apply false
-    kotlin("android") version "1.8.22" apply false
+import com.aliucord.gradle.AliucordExtension
+import com.android.build.gradle.BaseExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/snapshots")
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.aliucord:gradle:main-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/snapshots")
+        maven("https://jitpack.io")
+    }
+}
+
+fun Project.aliucord(configuration: AliucordExtension.() -> Unit) =
+    extensions.getByName<AliucordExtension>("aliucord").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "com.aliucord.gradle")
+    apply(plugin = "kotlin-android")
+
+    aliucord {
+        updateUrl.set("https://raw.githubusercontent.com/rhyan57/SvClone/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/rhyan57/SvClone/builds/%s.zip")
+    }
+
+    android {
+        compileSdkVersion(33)
+        defaultConfig {
+            minSdk = 24
+            targetSdk = 33
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = freeCompilerArgs +
+                "-Xno-call-assertions" +
+                "-Xno-param-assertions" +
+                "-Xno-receiver-assertions"
+        }
+    }
 }
