ee85c89f357a62deeb0f0c1cf19bbed9a85c5326
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 16:46:23 -0300

    Create build.gradle.kts

---
+++ SvClone/build.gradle.kts
@@ -0,0 +1,15 @@
+version = "1.0.0"
+description = "Clona servidores Discord direto do celular. Baseado em https://bettercloner.vercel.app"
+
+aliucord {
+    author("rhyan57")
+    changeLog.set("""
+        v1.0.0 - Lancamento inicial
+        - Clonar cargos, canais, emojis, stickers, icone e banner
+        - Comando /clone-server
+        - Botao Clone Guild na aba do servidor
+        - Salvar midia como ZIP
+        - Salvar progresso automaticamente
+    """.trimIndent())
+    deploy.set(true)
+}
