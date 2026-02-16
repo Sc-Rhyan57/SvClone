1fd52e6329ae8be74240d2124bbcc1a77c0ad6d0
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 17:08:19 -0300

    Update build.gradle.kts

--- SvClone/build.gradle.kts
+++ SvClone/build.gradle.kts
@@ -1,8 +1,16 @@
+plugins {
+    id("com.aliucord.gradle")
+    id("com.android.library")
+    kotlin("android")
+}
+
 version = "1.0.0"
 description = "Clona servidores Discord direto do celular. Baseado em https://bettercloner.vercel.app"
 
 aliucord {
     author("rhyan57")
+    updateUrl.set("https://raw.githubusercontent.com/rhyan57/SvClone/builds/updater.json")
+    buildUrl.set("https://raw.githubusercontent.com/rhyan57/SvClone/builds/%s.zip")
     changeLog.set("""
         v1.0.0 - Lancamento inicial
         - Clonar cargos, canais, emojis, stickers, icone e banner
@@ -12,4 +20,18 @@ aliucord {
         - Salvar progresso automaticamente
     """.trimIndent())
     deploy.set(true)
+}
+
+android {
+    compileSdkVersion(33)
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 }
