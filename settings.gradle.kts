c9283780e0f972eed218feb9b218b4a5f2377097
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 17:25:08 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,2 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.aliucord.com/snapshots")
-        google()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        maven("https://maven.aliucord.com/snapshots")
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "SvClone"
 include(":SvClone")
