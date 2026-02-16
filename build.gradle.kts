d2b9fd02a334921d4730d8abe0f0b0b647f863bd
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 17:07:36 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,5 @@
-version = "1.0.0"
-description = "Clona servidores Discord direto do celular. Baseado em https://bettercloner.vercel.app"
-
-aliucord {
-    author("rhyan57")
-    changeLog.set("""
-        v1.0.0 - Lancamento inicial
-        - Clonar cargos, canais, emojis, stickers, icone e banner
-        - Comando /clone-server
-        - Botao Clone Guild na aba do servidor
-        - Salvar midia como ZIP
-        - Salvar progresso automaticamente
-    """.trimIndent())
-    deploy.set(true)
+plugins {
+    id("com.aliucord.gradle") version "1.2.0" apply false
+    id("com.android.library") version "7.4.2" apply false
+    kotlin("android") version "1.8.22" apply false
 }
